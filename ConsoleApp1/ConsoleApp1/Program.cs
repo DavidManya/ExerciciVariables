c46c3a27061b4225c1d2951e387a915ce74c6b9c
David Manyà <david.manya@gmail.com>
2020-01-13 10:32:36 +0100

    Pujada FASE 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //Fase 1
             string nom = "David";
             string cognom1 = "Manyà";
             string cognom2 = "Álvarez";
@@ -29,7 +29,22 @@ namespace ConsoleApp1
             Console.WriteLine($"Avui és {dia}/{mes}/{any}");
             Console.WriteLine($"Avui és {diae}/{mese}/{anye}");
 
+            //Fase 2
+            int anyinici = 1948;
+            int naixement;
+            int dif;
+            int traspas = 4;
+            int quants;
 
+            //Demanar que introdueixi data de naixement
+            Console.WriteLine("Introdueix la teva data de naixement i prem Enter");
+            naixement = Convert.ToInt32(Console.ReadLine());
+
+            //Calculem diferència d'anys
+            dif = naixement - anyinici;
+            //Calculem quants anys de traspàs hi ha entre 1948 i l'any de naixement introduït
+            quants = dif / traspas;
+            Console.WriteLine("Entre 1948 i el teu any de naixement hi ha " + quants + " anys de traspàs");
         }
     }
 }
